4ef03f695f7f1d97ac6cba733dc6424f1d1a3da4
Eu-Jin Ooi <Ooi@Eu-Jin.dev>
2021-05-28 14:54:25 -0700

    Fetch Images

--- RugID/fetchImages.py
+++ RugID/fetchImages.py
@@ -7,12 +7,15 @@ designRead = pd.read_csv(designCSV)
 designArray = designRead['Handle'].unique()
 for design in designArray:
     try:
-        os.mkdir(os.getcwd() + "\\designs\\"+design)
+        os.mkdir(os.getcwd() + "\\designs\\test\\"+design)
     except:
         print("Tried but already there")
     designFilter = designRead.loc[designRead['Handle'] == design]
     designFilterArray = designFilter['Image Src'].unique()
     for url in designFilterArray:
-        filename = wget.download(url)
-        os.rename(filename, os.path.join(os.getcwd(), "designs", design, filename))
-        print(filename)
+        try:
+            filename = wget.download(url)
+            os.rename(filename, os.path.join(os.getcwd(), "designs","test", design, filename))
+            print(filename)
+        except:
+            print("Couldn't download", filename, ". Oh well.")
